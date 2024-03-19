0af1c7502aabdbb92a41362fc5dbfee0d739af67
kira1433 <krovidiaashishchandra@gmail.com>
2024-03-20 01:54:11 +0530

    Old Working Code

--- DM/modules/text.py
+++ DM/modules/text.py
@@ -34,20 +34,21 @@ def get_bert():
 # tokenize
 
 def tokenize(texts, add_special_tokens=True):
-    if not isinstance(texts, (list, tuple)):
-        texts = [texts]
+    # if not isinstance(texts, (list, tuple)):
+    #     texts = [texts]
 
-    tokenizer = get_tokenizer()
+    # tokenizer = get_tokenizer()
 
-    encoding = tokenizer.batch_encode_plus(
-        texts,
-        add_special_tokens=add_special_tokens,
-        padding=True,
-        return_tensors='pt'
-    )
+    # encoding = tokenizer.batch_encode_plus(
+    #     texts,
+    #     add_special_tokens=add_special_tokens,
+    #     padding=True,
+    #     return_tensors='pt'
+    # )
 
-    token_ids = encoding.input_ids
-    return token_ids
+    # token_ids = encoding.input_ids
+    # return token_ids
+    return None
 
 
 # embedding function
@@ -59,31 +60,32 @@ def bert_embed(
         eps=1e-8,
         pad_id=0.
 ):
-    model = get_bert()
-    mask = token_ids != pad_id
+    # model = get_bert()
+    # mask = token_ids != pad_id
 
-    if torch.cuda.is_available():
-        token_ids = token_ids.cuda()
-        mask = mask.cuda()
+    # if torch.cuda.is_available():
+    #     token_ids = token_ids.cuda()
+    #     mask = mask.cuda()
 
-    outputs = model(
-        input_ids=token_ids,
-        attention_mask=mask,
-        output_hidden_states=True
-    )
+    # outputs = model(
+    #     input_ids=token_ids,
+    #     attention_mask=mask,
+    #     output_hidden_states=True
+    # )
 
-    hidden_state = outputs.hidden_states[-1]
+    # hidden_state = outputs.hidden_states[-1]
 
-    if return_cls_repr:
-        return hidden_state[:, 0]  # return [cls] as representation
+    # if return_cls_repr:
+    #     return hidden_state[:, 0]  # return [cls] as representation
 
-    if not exists(mask):
-        return hidden_state.mean(dim=1)
+    # if not exists(mask):
+    #     return hidden_state.mean(dim=1)
 
-    mask = mask[:, 1:]  # mean all tokens excluding [cls], accounting for length
-    mask = rearrange(mask, 'b n -> b n 1')
+    # mask = mask[:, 1:]  # mean all tokens excluding [cls], accounting for length
+    # mask = rearrange(mask, 'b n -> b n 1')
 
-    numer = (hidden_state[:, 1:] * mask).sum(dim=1)
-    denom = mask.sum(dim=1)
-    masked_mean = numer / (denom + eps)
-    return masked_mean
+    # numer = (hidden_state[:, 1:] * mask).sum(dim=1)
+    # denom = mask.sum(dim=1)
+    # masked_mean = numer / (denom + eps)
+    # return masked_mean
+    return torch.Tensor(0,)
--- demo/demo_mug.py
+++ demo/demo_mug.py
@@ -1,6 +1,6 @@
 # demo on MUG dataset
 import sys
-sys.path.append("/workspace/code/CVPR23_LFDM")  # change this to your code directory
+sys.path.append("/home/zeta/Workbenches/Diffusion/CVPR23_LFDM")  # change this to your code directory
 import argparse
 
 import imageio
@@ -19,8 +19,8 @@ import cv2
 import matplotlib.pyplot as plt
 
 start = timeit.default_timer()
-root_dir = '/data/hfn5052/text2motion/cvpr23/demo_mug'
-GPU = "2"
+root_dir = '/home/zeta/Workbenches/Diffusion/CVPR23_LFDM/demo_mug'
+GPU = "3"
 postfix = "-j-sl-random-of-tr-rmm"
 INPUT_SIZE = 128
 N_FRAMES = 40
@@ -28,10 +28,10 @@ RANDOM_SEED = 1234
 MEAN = (0.0, 0.0, 0.0)
 cond_scale = 1.
 # downloaded the pretrained DM model and put its path here
-RESTORE_FROM = "/data/hfn5052/text2motion/videoflowdiff_mug/snapshots-j-sl-random-of-tr-rmm/flowdiff_0005_S111600.pth"
+RESTORE_FROM = "/home/zeta/Workbenches/Diffusion/CVPR23_LFDM/weights/DM_MUG.pth"
 # downloaded the pretrained LFAE model and put its path here
-AE_RESTORE_FROM = "/data/hfn5052/text2motion/RegionMM/log-mug/mug128/snapshots/RegionMM_0100_S046500.pth"
-config_path = "/workspace/code/CVPR23_LFDM/config/mug128.yaml"
+AE_RESTORE_FROM = "/home/zeta/Workbenches/Diffusion/CVPR23_LFDM/weights/LFAE_MUG.pth"
+config_path = "/home/zeta/Workbenches/Diffusion/CVPR23_LFDM/config/mug128.yaml"
 CKPT_DIR = os.path.join(root_dir, "demo"+postfix)
 os.makedirs(CKPT_DIR, exist_ok=True)
 print(root_dir)
@@ -107,7 +107,7 @@ def main():
     exp_list = ['anger', 'disgust', 'fear', 'happiness',
                 'neutral', 'sadness', 'surprise']
 
-    ref_img_path = "/workspace/code/CVPR23_LFDM/demo/mug_examples/img_0000.jpg"
+    ref_img_path = "/home/zeta/Workbenches/Diffusion/CVPR23_LFDM/demo/mug_examples/img_0000.jpg"
     print("input image:", ref_img_path)
     ref_img_name = os.path.basename(ref_img_path)[:-4]
     ref_img_npy = imageio.v2.imread(ref_img_path)[:, :, :3]
